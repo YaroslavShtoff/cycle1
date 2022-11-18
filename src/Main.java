51227e3f8106e7e79920c8e87e23efc9f0cda7ae
yarik <yarikshtoda1988@gmail.com>
2022-11-18 15:37:33 +0300

    HW Cycle

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,7 @@ public class Main {
 
         // Заданиу 5
 
-        for (int year = 1904; year <= 2096; year = year + 4)
+        for (int year = 1904; year <= 2097; year = year + 4)
             System.out.println(year + " год является високосным ");
 
         //Задание 6
